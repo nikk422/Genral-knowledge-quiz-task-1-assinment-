a9ff38ad2aa6a0a8864329f32169dc871e7fa841
Nikhil Malviya <94277199+nikk422@users.noreply.github.com>
2021-12-05 13:43:20 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,41 @@
+var readline=require("readline-sync");
+score=0
+
+
+var userName=readline.question("Enter your Name :")
+console.log("hello " , userName, "\n please play this game and improve your knowledge")
+
+
+function play(question,answer){
+  var userAnswer=readline.question(question)
+  if (userAnswer.toUpperCase()===answer.toUpperCase()){
+    console.log("your answer is right")
+    console.log("________")
+    score=score+1
+  }else{
+    console.log("your answer is wrong")
+    console.log("_______")
+  }
+};
+
+var AllQuestions=[{
+  question:"who is Yuvraj sigh? \n1. Cricketer \n2. Rider \n3. reporter \n4. poilet\n",
+  answer:"1"
+  },{
+    question:"who is prime minister of india? \n1. Narendra Modi \n2. Amit shaah \n3. Sachin tendulakar \n4. Amir khan\n",
+    answer:"1"
+  },{
+    question:"Where is TaajMahal? \n1.Delhi \n2.Indore \n3.Aagra \n4.Mumbai \n",
+    answer:"3"
+  },{
+    question:"Who is Big-B of Bollywood? \n1. Akshay kumar \n2. Shahrukh khan \n3. Amitabh Bachchan \n4. Dharmendra \n",
+    answer:"3"
+  }];
+
+for (var i=0; i<AllQuestions.length;i++){
+  var questionlist=AllQuestions[i]
+  play(questionlist.question,questionlist.answer)
+};
+
+
+console.log(userName, "Your final score is ", score)
